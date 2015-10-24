0bfb41b431870389dc99894d26e036f3c27edf0a
Dragos Rosu <r.dragosh@yahoo.com>
2015-10-24 19:38:22 +0100

    feat: added renderer, needs token fix

---
+++ Solution/Common/Credentials.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class Credentials
+    {
+        public static string ConsumerKey;
+        public static string SecretKey;
+        public static string AutodeskBaseUrl;
+
+        public static void Init(string consumerKey, string secretKey, string autodeskBaseUrl)
+        {
+            ConsumerKey = consumerKey;
+            SecretKey = secretKey;
+            AutodeskBaseUrl = autodeskBaseUrl;
+        }
+    }
+}
---
+++ Solution/Web.UI/GetAccessToken.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AutoDesk.Service;
+
+namespace Web.UI
+{
+    public partial class GetAccessToken : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var accessTokenRetriever = new AccessTokenRetriever();
+
+            var accessToken = accessTokenRetriever.GetAccessToken();
+
+            Response.ContentType = "application/json";
+
+            Response.Write(accessToken);
+            Response.End();
+        }
+    }
+}
