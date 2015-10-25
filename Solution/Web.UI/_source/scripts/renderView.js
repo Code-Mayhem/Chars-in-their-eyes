4c7eb2cd6238067bc1ea7cfd42f03088fcee3fec
Matthew Macartney <matthew.macartney@codecomputerlove.com>
2015-10-25 06:56:39 +0000

    Feat: added handlebars

---
+++ Solution/Web.UI/_source/scripts/renderView.js
@@ -0,0 +1,54 @@
+﻿(function ($, window, undefined) {
+
+	"use strict";
+	window.code = window.code || {};
+	var renderView = window.code.renderView = function (options) {
+
+		var temp = '[data-handlebars]';
+		var render = '[data-render]';
+
+		function init() {
+			handleEvent();
+
+			getData();
+		}
+
+		function handleEvent() {
+			
+		}
+
+		function getData() {
+			var smsRenderHub = $.connection.smsRenderHub;
+
+			smsRenderHub.client.addNewSmsRenders = function (renderModel) {
+				console.log(renderModel);
+
+				handlebarsObject(renderModel);
+			};
+
+			$.connection.hub.start().done(function () {
+				smsRenderHub.server.sendNewSmsRenders();
+			});
+
+			//console.log(data);
+
+			//return data;
+		}
+
+		function handlebarsObject(data) {
+				
+			var source = $(temp).html();
+			var template = Handlebars.compile(source);
+
+			$(render).append(template(data));
+
+		}
+
+		init();
+	};
+
+}(jQuery, window));
+
+$(function () {
+	var renderView = new code.renderView();
+});
