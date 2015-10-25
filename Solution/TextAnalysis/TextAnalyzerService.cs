10929049710265209eef86c76bc99e7133a2344b
Dragos Rosu <r.dragosh@yahoo.com>
2015-10-25 09:07:59 +0000

    feat: fixed loading

--- Solution/TextAnalysis/TextAnalyzerService.cs
+++ Solution/TextAnalysis/TextAnalyzerService.cs
@@ -13,41 +13,78 @@ namespace TextAnalysis
     {
 	    public string AnalyzeText(string text)
 	    {
-		    if (text == null || text.Length < 5) text = "empty";
+	        if (text == null || text.Length < 5)
+	        {
+	            text = "empty";
+	        }
 
-				var alchemyObj = new AlchemyAPI();
-				alchemyObj.LoadAPIKey("2bfb53d6c8ac06b2ac12e6a0d3766d66b2aba44b");
+			var response = CallAlchemyApiAnalysisOnText(text);
 
-				var json = alchemyObj.TextGetRankedKeywords(text);
+            if (response == null || !response.Keywords.Any())
+	        {
+	            return "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Y2hhcnNpbnRoZWlyZXllcy9haXJwbGFuZS5kd2c="; // blank model
+	        }
 
-				// Get keywords from json
-				var response = JsonConvert.DeserializeObject<AlchemyRs>(json);
-				
-				var filePath = string.Format("c:\\models.json");
+	        foreach (var model in RenderedModelsRepository.ModelList.Models)
+	        {
+	            var tags = model.Tags.Split(',');
+	            var intersectTagsWithKeywords = response.Keywords.Select(k => k.Text).Intersect(tags);
+	            if (intersectTagsWithKeywords.Any())
+	            {
+	                return model.FileUrn;
+	            }
+	        }
 
-				var reader = new StreamReader(filePath);
-				var jsonReader = new JsonTextReader(reader);
-				var serializer = new JsonSerializer();
-				var modelList = serializer.Deserialize<ModelList>(jsonReader);
-				jsonReader.Close();
+	        return "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Y2hhcnNpbnRoZWlyZXllcy9haXJwbGFuZS5kd2c="; // blank model
 
-		    foreach (var keyword in response.Keywords)
-		    {
-			    foreach (var model in modelList.Models)
-			    {
-				    var tags = model.Tags.Split(',');
-				    foreach (var tag in tags.Where(tag => tag.Trim() == keyword.Text))
-				    {
-					    return model.FileUrn;
-				    }
-			    }
-		    }
-				return "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Y2hhcnNpbnRoZWlyZXllcy9BdWRpK1I4LmRhZQ=="; // blank model
+        }
+
+	    private static AlchemyRs CallAlchemyApiAnalysisOnText(string text)
+	    {
+	        var alchemyObj = new AlchemyAPI();
+	        alchemyObj.LoadAPIKey("2bfb53d6c8ac06b2ac12e6a0d3766d66b2aba44b");
+
+	        var json = alchemyObj.TextGetRankedKeywords(text);
+
+	        // Get keywords from json
+	        var response = JsonConvert.DeserializeObject<AlchemyRs>(json);
+	        return response;
 	    }
     }
 
     public interface ITextAnalyzerService
     {
         string AnalyzeText(string text);
+    }
+
+    public static class RenderedModelsRepository
+    {
+        private static ModelList _modelList;
+
+        public static ModelList ModelList
+        {
+            get
+            {
+                if (_modelList == null)
+                {
+                    _modelList = ReadFile();
+                }
+
+                return _modelList;
+            }
+        }
+
+        private static ModelList ReadFile()
+        {
+            var filePath = string.Format("c:\\models.json");
+
+            var reader = new StreamReader(filePath);
+            var jsonReader = new JsonTextReader(reader);
+            var serializer = new JsonSerializer();
+            var modelList = serializer.Deserialize<ModelList>(jsonReader);
+            jsonReader.Close();
+
+            return modelList;
+        }
     }
 }
