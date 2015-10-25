1f70f66f392718dac30ae4d2fd5084d55ba68f10
Dragos Rosu <r.dragosh@yahoo.com>
2015-10-25 11:01:37 +0000

    refactoring

--- Solution/TextAnalysis/TextAnalyzerService.cs
+++ Solution/TextAnalysis/TextAnalyzerService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Common;
 
 namespace TextAnalysis
 {
@@ -63,5 +65,36 @@ namespace TextAnalysis
     public interface ITextAnalyzerService
     {
         string AnalyzeText(string text);
+    }
+
+    public static class RenderedModelsRepository
+    {
+        private static ModelList _modelList;
+
+        public static ModelList ModelList
+        {
+            get
+            {
+                if (_modelList == null)
+                {
+                    _modelList = ReadFile();
+                }
+
+                return _modelList;
+            }
+        }
+
+        private static ModelList ReadFile()
+        {
+            var filePath = string.Format("c:\\models.json");
+
+            var reader = new StreamReader(filePath);
+            var jsonReader = new JsonTextReader(reader);
+            var serializer = new JsonSerializer();
+            var modelList = serializer.Deserialize<ModelList>(jsonReader);
+            jsonReader.Close();
+
+            return modelList;
+        }
     }
 }
