922ce1f27004641bae9929db9ed2531735853935
Dragos Rosu <r.dragosh@yahoo.com>
2015-10-25 04:57:57 +0000

    feat: link all projs

--- Solution/Web.UI/Controllers/HomeController.cs
+++ Solution/Web.UI/Controllers/HomeController.cs
@@ -10,13 +10,29 @@ namespace Web.UI.Controllers
 	using Web.UI.ViewModels;
 
 	public class HomeController : Controller
-    {
+	{
+	    public SmsAnalyzer SmsAnalyzer { get; set; }
+
+        public List<SmsRenderModel> RenderModels { get; set; } 
+
+	    public HomeController()
+	    {
+	        SmsAnalyzer = SmsAnalyzer.Instance;
+	        RenderModels = new List<SmsRenderModel>();
+	        SmsAnalyzer.Publish += () =>
+	        {
+	            RenderModels = SmsAnalyzer.SmsRenderModelsCache;
+	            var json = SmsAnalyzer.SmsRenderModelsAsJson;
+	        };
+	    }
+
 	    //
         // GET: /Home/
-	    public ActionResult Index()
+	    public ActionResult Index(IEnumerable<SmsRenderModel> renderModel)
 	    {
-            return View(new ModelList(){Models = new List<Model>(){new Model(){ FileName = "abc"}}});
-	    }
+            //return View(new ModelList(){Models = new List<Model>(){new Model(){ FileName = "abc"}}});
+            return View(RenderModels);
+        }
 
 			public ActionResult Analyse()
 			{
