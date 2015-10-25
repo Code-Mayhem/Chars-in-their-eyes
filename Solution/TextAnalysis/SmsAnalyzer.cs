a82412b7e39bfab982e37324734bcb5f49ddf3c5
Dragos Rosu <r.dragosh@yahoo.com>
2015-10-25 03:43:46 +0000

    feat: added an sms analyzer

---
+++ Solution/TextAnalysis/SmsAnalyzer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClockworkSMS.Service;
+
+namespace TextAnalysis
+{
+    public class SmsAnalyzer
+    {
+        private SmsAnalyzer _instance;
+        public IOrderedEnumerable<KeyValuePair<string, SMS>> SmsCache { get; set; } 
+        public List<string> ProcessedSmsHashes { get; set; }
+        public List<SmsRenderModel> SmsRenderModelsCache { get; set; } 
+
+        private ExternalApiSmsService ExternalApiSmsService { get; set; }
+
+        public SmsAnalyzer Instance
+        {
+            get { return _instance ?? (_instance = new SmsAnalyzer()); }
+        }
+
+        private SmsAnalyzer()
+        {
+            this.ExternalApiSmsService = ExternalApiSmsService.Instance;
+            this.SmsRenderModelsCache = new List<SmsRenderModel>();
+            ExternalApiSmsService.Publish += () =>
+            {
+                this.SmsCache = ExternalApiSmsService.GetAllSms().OrderBy(s => s.Value.Timestamp);
+                var unprocessedSms = SmsCache.Except(SmsCache.Where(s => ProcessedSmsHashes.Contains(s.Key)));
+
+                var smsRenderModels = ProcessSms(unprocessedSms);
+                ProcessedSmsHashes.AddRange(smsRenderModels.Select(s => s.Sms.Hash));
+                SmsRenderModelsCache.AddRange(smsRenderModels);
+
+            };
+        }
+
+        public IEnumerable<SmsRenderModel> ProcessSms(IEnumerable<KeyValuePair<string, SMS>> unprocessedSms)
+        {
+            return new List<SmsRenderModel>();
+        }
+    }
+}
