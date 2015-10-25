e6fb299b3f7c08c5d6bb00eb63cb06974516b56f
Matthew Macartney <matthew.macartney@codecomputerlove.com>
2015-10-25 09:52:22 +0000

    Feat: I have officially added thumbnails, bitchin 20k15

--- Solution/Web.UI/_source/scripts/renderView.js
+++ Solution/Web.UI/_source/scripts/renderView.js
@@ -25,23 +25,50 @@
 				var dec = decodeURI(renderModel);
 				var test = JSON.parse(dec);
 
+
+				//console.log(test);
 				
 				for (var i = 0; i < test.length; i++) {
 
-					var url = 'https://developer.api.autodesk.com/viewingservice/v1/thumbnails/' + test[i].Urn;
+					var urnCode = test[i].Urn;
+
+					var url = 'https://developer.api.autodesk.com/viewingservice/v1/thumbnails/' + urnCode;
 
 					var req = new XMLHttpRequest();
 					req.open("GET", url, false);
+					req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+					req.setRequestHeader('Authorization', 'Bearer' + getToken());
 					req.send(null);
-					console.log(req.responseText);
+					//console.log(req.responseText);
+
+					test[i].thumbNail = url;
 				}
 				
+				console.log(test);
+
 				handlebarsObject(test);
 			};
 
 			$.connection.hub.start().done(function () {
 				smsRenderHub.server.sendNewSmsRenders();
 			});
+		}
+
+		function getToken() {
+
+			var xmlHttp = null;
+			xmlHttp = new XMLHttpRequest();
+			xmlHttp.open("GET", "GetAccessToken.aspx", false);
+			xmlHttp.send(null);
+			var res = xmlHttp.responseText;
+			var newToken = JSON.parse(res);
+
+			if (newToken.error) {
+				console.log(newToken.error);
+				return;
+			}
+
+			return newToken.access_token;
 		}
 
 		function handlebarsObject(data) {
